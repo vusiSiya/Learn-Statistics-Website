23b6ed59adc866af14523947f6e95207abb2e6bc
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-06-24 00:27:50 +0200

    Update mean.js_using functional programming

--- Calculators/Mean/mean.js
+++ Calculators/Mean/mean.js
@@ -1,4 +1,4 @@
-const dataSamples = document.querySelector(".samples")
+const dataValuesEl = document.querySelector(".data-values")
 const sampleSize = document.querySelector(".sample-Size")
 const samplesBtn = document.querySelector(".btn-Enter-samples")
 let inputIndexes = []
@@ -7,10 +7,12 @@ let sampleName
 let isChanged = false;
 let sum = 0, mean = 0, range = 0, standardDeviation = 0;
 
+const newArray = (_array) => _array.map(value => value *= 0);
+const getDefinedValues = (_array) => _array.filter( num => num != null);
 
 samplesBtn.addEventListener("click", ()=>{
 	isChanged = true;
-	enterSamples()
+	enterSampleData()
 })
 
 document.addEventListener("change", (e) => {
@@ -18,103 +20,102 @@ document.addEventListener("change", (e) => {
 		return  handleValueChange(e)
 	}
 	else if(e.target.className=== "sample-Size"){
-		return isChanged ?  enterSamples() : isChanged
+		return isChanged ?  enterSampleData() : isChanged
 	}
 }) 
-function enterSamples() {
+function enterSampleData() {
 	 
 	sum = 0, mean = 0, range = 0, standardDeviation = 0;
 	let Length = sampleSize.value * 1;
 	
-	cleanUpDataValuesArray() 
-	
+	cleanUpHTML();
+	dataValuesArray = newArray(dataValuesArray)
+	inputIndexes = newArray(inputIndexes)
+
 	for (let i = 0; i < Length; i++)
 	{	
 		sampleName = `sample-${i + 1}`
 		inputIndexes[i] = `${sampleName}`
-		dataSamples.innerHTML += createInputEl(i) // the function that creates the input elements 		
+		dataValuesEl.innerHTML += createInputEl(i) // the function that creates the input elements 		
 	}		
 	//(inputIndexes.length > 0) ? clearInputIndexes() : createInputIndexes()	
 }
 
 function createInputEl(i) {
-	let sInput = `
+	let _s = `
 	<input type="number" class="inputEl" 
 		 name =${inputIndexes[i]} placeholder="value" />`
-	return sInput;
+	return _s;
 }
 
-function cleanUpDataValuesArray() {
+function cleanUpHTML() {
 	document.querySelector(".p--data").innerText="data: "
-	dataSamples.innerHTML = ""
-	for (let k = 0; k < dataValuesArray.length; k++) {
-		dataValuesArray[k] = 0;
-		if ( k=== dataValuesArray.length - 1) {
-			displayOutput()
-		}
-	}
+	dataValuesEl.innerHTML = ""
+	displayOutput()
 }
 
 function handleValueChange(event) {
-	const { value, name } = event.target
-	console.log(event)
-	for (let i = 0; i < inputIndexes.length; i++) {
+	const { value, name } = event.target;
+	/*for (let i = 0; i < inputIndexes.length; i++) {
 		if ( name === inputIndexes[i]) {
 			dataValuesArray[i] = value * 1
 		}
-	}
-	sum = Sum()		
-	console.log(sum)
-	mean = Mean(sum)
-	range = Range()
-	standardDeviation = StandardDeviation(mean)
-	
-	displayOutput() 
+	}*/
+	let isWantedValue = inputIndexes.includes(name);
+	inputIndexes.forEach( (string,i) =>{
+		if(string === name){
+			dataValuesArray[i] = value * 1;
+		};	
+	})
+	const definedValues = getDefinedValues(dataValuesArray)
+
+	sum = Sum(definedValues);		
+	mean = Mean(sum, definedValues);
+	range = Range(definedValues);
+	standardDeviation = StandardDeviation(mean, definedValues);
+	displayOutput(); 
 }
 
-function Sum() {
-	let num = 0;
-	for (let i = 0; i < dataValuesArray.length; i++) {
-		num += (dataValuesArray[i] ?  dataValuesArray[i] : 0 )
-	}
-	return num
+function Sum(_array) {
+	let _sum = _array.reduce((acc, num) => {
+		return acc + num
+	}, 0)
+	return _sum
 }
 
-function Mean(sum) {
-	mean = sum / dataValuesArray.length;
-	
-	return mean.toFixed(2)
+function Mean(_sum,_definedValues) {
+
+	//const definedValues = getDefinedValues(_definedValues)
+	let _mean = _sum / _definedValues.length;
+	return _mean.toFixed(2)
 }
 
-function StandardDeviation(mean) {
-	let stSum = 0
-	let arrayLength = dataValuesArray.length
-	for (let i = 0; i < arrayLength; i++) {
-		let currentValue = dataValuesArray[i]
-		let difference = currentValue ? currentValue - mean : 0 
-		stSum += Math.exp(difference, 2)
-	}
-	let n = arrayLength - 1
-	let standardDeviation = Math.sqrt((stSum /n), 2)
+function StandardDeviation(_mean, _definedValues) {
+	const array = _definedValues.map(number => Math.pow(number - _mean, 2));
+	const _sum = Sum(array);
+	let N = array.length - 1;
+	let standardDeviation = Math.sqrt((_sum / N), 2);
 	return standardDeviation.toFixed(2);
 }
 
-function Range() {
-	let arrayLength = dataValuesArray.length 
-	let smallestNum = dataValuesArray[0] ? dataValuesArray[0]: 0;
-	let highestNum = dataValuesArray[0] ? dataValuesArray[0]: 0;
-
-	for (let i = 0; i < arrayLength; i++) {
-		let num = dataValuesArray[i]
-		if (num <= smallestNum) {
-			smallestNum = num
-		}
-		else if (num > highestNum) {
-			highestNum = num 
-		}
+//const LoopOverArray = (_array, func) => _array.forEach(func);
+function Range(_definedValues) {	
+	const arrangeElements = (_array) => {
+		let smallestNum = 0;
+		const nArray = _array.map(num =>{
+			if(num < smallestNum ){
+				_array.unshift(num)
+				smallestNum = num
+			}
+			else{
+				_array.shift(num)
+			}
+		})	
 	}
-	let range = highestNum - smallestNum
-	return range
+	const newArray = arrangeElements(_definedValues)
+	const lastEl = newArray.length - 1;
+	let _range = newArray[lastEl] - newArray[0];
+	return _range;
 }
 
 function  displayOutput() {
