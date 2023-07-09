634f778250968d980c5af46887f84b380f0e9411
Siyabonga Mahlalela <siyabonga.mahlalela@imaginescholar.org>
2023-07-09 17:15:16 +0200

    Finally fixing the repeating quizzes issue!!

--- Calculators/Mean/mean.js
+++ Calculators/Mean/mean.js
@@ -5,42 +5,47 @@ let inputIndexes = []
 let dataValuesArray = []
 let sampleName
 let isChanged = false;
-let sum = 0, mean = 0, range = 0, standardDeviation = 0;
+let sum = 0
+  , mean = 0
+  , range = 0
+  , standardDeviation = 0;
 
-const newArray = (_array) => _array.map(value => value *= 0);
+const newArray = (_array) => _array.map( value => value *= 0);
 const getDefinedValues = (_array) => _array.filter( num => num != null);
 
 samplesBtn.addEventListener("click", ()=>{
 	isChanged = true;
-	enterSampleData()
-})
+	enterSampleData();
+}
+)
 
-document.addEventListener("change", (e) => {
+document.addEventListener("change", (e)=>{
 	const {className} = e.target;
-	
-	if (className=== "inputEl"){
-		handleValueChange(e);
-	}
-	else if(className=== "sample-Size"){
-		isChanged ?  enterSampleData() : isChanged;
-	}
-}) 
-function enterSampleData() {
-	 
-	sum = 0, mean = 0, range = 0, standardDeviation = 0;
-	let Length = sampleSize.value * 1;
-	
-	cleanUpHTML();
-	dataValuesArray = newArray(dataValuesArray)
-	inputIndexes = newArray(inputIndexes)
 
-	for (let i = 0; i < Length; i++)
-	{	
+	if (className === "inputEl") {
+		handleValueChange(e);
+	} else if (className === "sample-Size") {
+		isChanged ? enterSampleData() : isChanged;
+	}
+}
+)
+function enterSampleData() {
+
+	sum = 0,
+	mean = 0,
+	range = 0,
+	standardDeviation = 0;
+	let Length = sampleSize.value * 1;
+
+	cleanUpHTML();
+	dataValuesArray = newArray(dataValuesArray);
+	inputIndexes = newArray(inputIndexes);
+
+	for (let i = 0; i < Length; i++) {
 		sampleName = `sample-${i + 1}`
 		inputIndexes[i] = `${sampleName}`
-		dataValuesEl.innerHTML += createInputEl(i) // the function that creates the input elements 		
-	}		
-	//(inputIndexes.length > 0) ? clearInputIndexes() : createInputIndexes()	
+		dataValuesEl.innerHTML += createInputEl(i)	
+	}
 }
 
 function createInputEl(i) {
@@ -51,70 +56,69 @@ function createInputEl(i) {
 }
 
 function cleanUpHTML() {
-	document.querySelector(".p--data").innerText="data: "
+	document.querySelector(".p--data").innerText = "data: "
 	dataValuesEl.innerHTML = ""
 	displayOutput()
 }
 
 function handleValueChange(event) {
-	const { value, name } = event.target;
+	const {value, name} = event.target;
 	//let isWantedValue = inputIndexes.includes(name);
-	inputIndexes.forEach( (string,i) =>{
-		if(string === name){
+	inputIndexes.forEach((string,i) =>{
+		if (string === name) {
 			dataValuesArray[i] = value * 1;
-		};	
+		}
 	})
 	const definedValues = getDefinedValues(dataValuesArray)
-	sum = Sum(definedValues);		
+	sum = Sum(definedValues);
 	mean = Mean(sum, definedValues);
 	range = Range(definedValues);
 	standardDeviation = StandardDeviation(mean, definedValues);
-	displayOutput(); 
+	displayOutput();
 }
 
 function Sum(_array) {
-	let _sum = _array.reduce((acc, num) => {
-		return acc + num
-	}, 0)
+	let _sum = _array.reduce((acc,num) =>{ acc + num}, 0);
 	return _sum
 }
 
-function Mean(_sum,_definedValues) {
+function Mean(_sum, _definedValues) {
 	let _mean = _sum / _definedValues.length;
 	return _mean.toFixed(2)
 }
 
 function StandardDeviation(_mean, _definedValues) {
-	const array = _definedValues.map(number => Math.pow(number - _mean, 2));
+	const array = _definedValues.map( number => Math.pow(number - _mean, 2));
 	const _sum = Sum(array);
 	let N = array.length - 1;
 	let standardDeviation = Math.sqrt((_sum / N), 2);
 	return standardDeviation.toFixed(2);
 }
 
-function Range(_definedValues) {	
-	const arrangeElements = (_array) => {
-		let smallestNum = _array[0]; let highestNum = _array[0];
-		const nArray =[]
-		 _array.map(num =>{
-			if(num < highestNum ){
+function Range(_definedValues) {
+	const arrangeElements = (_array)=>{
+		let smallestNum = _array[0];
+		let highestNum = _array[0];
+		const nArray = []
+		_array.map( num =>{
+			if (num < highestNum) {
 				nArray.unshift(num);
-				smallestNum = num	
-			}
-			else{
+				smallestNum = num
+			} else {
 				nArray.push(num);
-				highestNum = num	
+				highestNum = num
 			}
 		})
 		return nArray
 	}
-	const newArray = arrangeElements(_definedValues) // OR _definedValues.slice().sort( (a,b) => a > b ? 1 : -1)
+	const newArray = arrangeElements(_definedValues)
+	// OR _definedValues.slice().sort( (a,b) => a > b ? 1 : -1)
 	const lastEl = newArray.length - 1;
 	let _range = newArray[lastEl] - newArray[0];
 	return _range;
 }
 
-function  displayOutput() {
+function displayOutput() {
 	let s_ = `
 	<section class="sought-value">
 		<p class="sum-El">Sum:${sum}</p>
@@ -124,6 +128,6 @@ function  displayOutput() {
 			Standard deviation: ${standardDeviation}
 		</p>
 	</section>
-		`		
+		`
 	document.querySelector(".output").innerHTML = s_
 }
--- index.js
+++ index.js
@@ -1,71 +1,64 @@
-
-
 //const displayCalculator = document.querySelector(".calculatorDisplay")
 
 /*import {createServer} from "miragejs"*/
 const quizCards = document.querySelector(".cards");
 const anotherCardBtn = document.querySelector(".btn--another-quiz");
-const quizData= [
-    {   
-        topic: `Hypothesis Testing`,
-        question:`Practical 1. Q1. A manufacturer claims that his market share is 60%.
+const quizData = [{
+	topic: `Hypothesis Testing`,
+	question: `Practical 1. Q1. A manufacturer claims that his market share is 60%.
             However a random sample of 500 customers reveals that only 275 are users of his product.
             Test the manufacturer’s claim at the 1% level of significance. `,
-        answer: `HEllo World`
-    },    
-    {  
-        topic:`Confidence Interval`,
-        question:`5.	A tyre manufacturer found that the sample mean tread life of 49 radial tyres tested was 52345 km with standard deviation 12943 km. <br/>
+	answer: `HEllo World`
+}, {
+	topic: `Confidence Interval`,
+	question: `5.	A tyre manufacturer found that the sample mean tread life of 49 radial tyres tested was 52345 km with standard deviation 12943 km. <br/>
             Construct a 99% confidence interval estimate for the true mean tread life of all radial tyres manufactured.`,
-        answer: ``
-    },    
-    {	        
-        topic:'ANOVA' ,
-        question:`
+	answer: ``
+}, {
+	topic: 'ANOVA',
+	question: `
             Complete the analysis of variance table and test whether for the difference amongst the treatment (Factor A) means and block (Factor B) means. Test at 10% significance level.           
             `,
-        answer: `HEllo World`
-    },    
-    {	        
-        topic:'Chi-Square' ,
-        question:`Conduct a goodness of fit test at a 1% level of significance to see whether the following sample appears to have been selected from a normal distribution.
+	answer: `HEllo World`
+}, {
+	topic: 'Chi-Square',
+	question: `Conduct a goodness of fit test at a 1% level of significance to see whether the following sample appears to have been selected from a normal distribution.
             The sample mean and sample standard deviation are 69 and 17.8237 respectively: <br/>
             50  	80  	90   	50   	55   	90   	55    	50   	70   	95
             90    	60   	85     	58   	60   	98   	62   	80	     62	    40
         `,
-        answer: `HEllo World`
-    },   
-    {	       
-        topic:'Rank Sum Test' ,
-        question:``,
-        answer: `HEllo World`
-    }   
-]
+	answer: `HEllo World`
+}, {
+	topic: 'Rank Sum Test',
+	question: ``,
+	answer: `HEllo World`
+}]
 //let server = createServer()
 //server.get("/api/users", { quizes: [quizData] })
 
-let indexes = [0] 
+let indexes = [0]
 let cardIsClicked = false;
-let answer 
-let currentCardData
+let answer
 let randomIndex
+let currentCardData
 let isNewCard = false;
 
 initialCardsDisplay();
 
 function initialCardsDisplay() {
-     quizCards.innerHTML = getCards(quizData);    
+	quizCards.innerHTML = getCards(quizData);
 }
 
 function getCards(data) {
-    let s = ""  
-    data.map( (quiz, i) => s += newCard(quiz, i))
-    return s; 
+	let s = ""
+	data.map((quiz,i)=>s += newCard(quiz, i))
+	return s;
 }
 
 function newCard(props, i) {
-    let arrayLength = quizCards.children.length
-    return `
+	let arrayLength = quizCards.children.length;
+	let elementCount = isNewCard ? arrayLength : i;
+	return `
         <div class="card " >
                 <h3 class = 'card--topic' >${props.topic}</h3>
                 <div class="card-content">
@@ -74,58 +67,56 @@ function newCard(props, i) {
                         ${props.question}
                     </p>
                 </div>
-                <button id = "${i}"class="solution ${isNewCard ? arrayLength : i }" >See Solution</button>
+                <button id = "${elementCount}" class="solution ${elementCount}" >See Solution</button>
         </div>`
-} 
- 
-anotherCardBtn.addEventListener("click", (e)=>{
-    const anotherCard = getCardIndex(indexes); 
-    isNewCard = true;
-    return quizCards.innerHTML +=  newCard(anotherCard, randomIndex);
-})
-
-function getCardIndex(_indexes){
-    //The purpose of this source code is to prevent the repeat of quizzes, when user clicks on the 'another quiz' button.
-    randomIndex =  Math.floor(Math.random() * quizData.length)
-    
-    _indexes.push(randomIndex)
-    let iLength = _indexes.length - 1
-    for (let j = iLength - 1; j > 0 ; j--) {
-        if (randomIndex === _indexes[j]  ) {
-            _indexes[j+1] = Math.floor(Math.random()* quizData.length) 
-            randomIndex = _indexes[j];
-        }        
-    }
-  
-    let randomCard = quizData[randomIndex]
-    return randomCard
 }
 
-document.addEventListener("click", (e)=> {
-    const {id,classList} = e.target
-    if (classList[0] === "solution") {
-        
-        let cardData = quizData[id];         
-        cardIsClicked = !cardIsClicked //returns the opposite value of the current cardIsClicked
-        
-        if (cardIsClicked) { 
-            answer = cardData.answer
-        } 
-        let cardIndex = classList[1] 
-        let cardContentEl = quizCards.children[cardIndex].children[1]
-        cardContentEl.innerHTML = updatedCardContent(cardData);
-    }
-     
+anotherCardBtn.addEventListener("click", ()=>{
+	const anotherCard = getCardIndex(indexes);
+	isNewCard = true;
+	return quizCards.innerHTML += newCard(anotherCard, randomIndex);
 })
 
-function updatedCardContent(props){
-    let value =  cardIsClicked ? 
-        `<pre>Solution:</pre>
-        <p class="answer"> ${answer}</p`
-        : 
-        `<pre>Quiz Question:</pre>
+function getCardIndex(_indexes) {
+	
+	const getIndex = ()=>Math.floor(Math.random() * quizData.length);
+	let _randomIndex = getIndex();
+	const check = (_randomIndex)=>{
+		let isRepeated = _indexes.includes(_randomIndex);
+		if (isRepeated === false) {
+			_indexes.push(_randomIndex);
+			return;
+		} else {
+			_randomIndex = getIndex();
+			check(_randomIndex);
+		}
+	}
+	check(_randomIndex);
+	randomIndex = _randomIndex; // syncing the global and local randomIndex, to use the global randomIndex variable in other functions;
+	let randomCard = quizData[_randomIndex];
+	return randomCard;
+}
+
+document.addEventListener("click", (e)=>{
+	const {id, classList} = e.target;
+	if (classList[0] === "solution") {
+
+		let cardData = quizData[id];
+		cardIsClicked = !cardIsClicked;
+
+		if (cardIsClicked) {
+			answer = cardData.answer;
+		}
+		let cardIndex = classList[1]
+		let cardContentEl = quizCards.children[cardIndex].children[1];
+		cardContentEl.innerHTML = updatedCardContent(cardData);
+	}
+})
+
+function updatedCardContent(props) {
+	let value = cardIsClicked ? `<pre>Solution:</pre>
+        <p class="answer"> ${answer}</p` : `<pre>Quiz Question:</pre>
             <p class='question'>${props.question}</p>`
-    
-    return value
-}
 
+	return value;
+}
